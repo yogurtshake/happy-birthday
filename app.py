588d5afee9439845758e9ee7bac3454221683864
yogurtshake <59886219+yogurtshake@users.noreply.github.com>
2025-09-21 22:06:24 -0400

    testing fix

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def init_db():
         )''')
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/happy-birthday/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form['name']
